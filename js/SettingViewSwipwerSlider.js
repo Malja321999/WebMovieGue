bbdb333d96846800171dd55278aae74edf4df220
JAMALUDIN <74660107+Malja321999@users.noreply.github.com>
2024-08-23 11:02:25 +0800

    23_8_24_Fix_Bug

---
+++ js/SettingViewSwipwerSlider.js
@@ -0,0 +1,39 @@
+/*Initialize Swiper*/
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
+/*--menu-btn-fixed-when-scroll============*/
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 20) {
+    $(".navigation").addClass("fix-icon");
+  } else {
+    $(".navigation").removeClass("fix-icon");
+  }
+});
---
+++ js/ViewMovieByCategory.js
@@ -0,0 +1,61 @@
+const categoryTitle = document.querySelectorAll(".category-title");
+console.log("cate:", categoryTitle.length);
+const allCategoryPosts = document.querySelectorAll(".post-box"); //ambil semua post-box tapi cuma bagian sebelah, class = "post-box movies"  jadi cuma bisa di ambil kategori di class langsunganya saja
+const paginationContainer = document.getElementById("pagination-container");
+const menu = ".post-box";
+
+for (let i = 0; i < categoryTitle.length; i++) {
+  categoryTitle[i].addEventListener(
+    "click",
+    filterPosts.bind(this, categoryTitle[i])
+  );
+}
+
+function filterPosts(item) {
+  changeActivePosition(item);
+  for (let i = 0; i < allCategoryPosts.length; i++) {
+    if (allCategoryPosts[i].classList.contains(item)) {
+      allCategoryPosts[i].style.display = "block";
+    } else {
+      allCategoryPosts[i].style.display = "none";
+    }
+  }
+}
+
+function filterPosts(item) {
+  changeActivePosition(item);
+  for (let i = 0; i < allCategoryPosts.length; i++) {
+    if (allCategoryPosts[i].classList.contains(item.attributes.id.value)) {
+      allCategoryPosts[i].style.display = "block";
+    } else {
+      allCategoryPosts[i].style.display = "none";
+    }
+  }
+}
+
+function changeActivePosition(activeItem) {
+  for (let i = 0; i < categoryTitle.length; i++) {
+    categoryTitle[i].classList.remove("active");
+  }
+  activeItem.classList.add("active");
+  paginationContainer.style.visibility = "hidden";
+}
+
+var items = $(`${menu}`);
+var numItems = items.length;
+console.log("numItems", numItems);
+
+var perPage = 8;
+
+items.slice(perPage).hide();
+$("#pagination-container").pagination({
+  items: numItems,
+  itemsOnPage: perPage,
+  prevText: "<",
+  nextText: ">",
+  onPageClick: function (pageNumber) {
+    var showFrom = perPage * (pageNumber - 1);
+    var showTo = showFrom + perPage;
+    items.hide().slice(showFrom, showTo).show();
+  },
+});
